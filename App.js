adebb04833a02f44fd1ec2c236e1ca4ec341ac3c
Mustafa <mjebbar17@gmail.com>
2018-08-10 01:01:27 +0100

    home screen in dev

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+
+    startGame = () => {
+        alert('starting game');
+    }
+
+    joinGame = () => {
+        alert('joining game');
+    }
+
+    render() {
+        return (
+           <View style={styles.container}>
+
+               <View>
+                   <Text style={styles.title}>Mafia</Text>
+               </View>
+
+               <View>
+                   <TouchableOpacity onPress={this.startGame} style={styles.button}>
+                       <Text>Start Game</Text>
+                   </TouchableOpacity>
+               </View>
+
+               <View>
+                   <TouchableOpacity onPress={this.joinGame} style={styles.button}>
+                       <Text>Join Game</Text>
+                   </TouchableOpacity>
+               </View>
+
+
+
+           </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        marginBottom: 40,
+        fontSize: 24
+    },
+    button: {
+        margin: 20,
+        padding: 20,
+        borderWidth: 1,
+        borderColor: 'lightgrey'
+    }
 });
