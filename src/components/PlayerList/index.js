52c56dab6afa4f4bd1aac5218f84afeaa0987d07
mahmoud <mahmoud16h@gmail.com>
2018-08-22 00:29:25 +0100

    Created PlayerList component

---
+++ src/components/PlayerList/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './PlayerList.css'
+
+class PlayerList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            players: this.props.players
+        }
+    }
+
+    getPlayers = () => {
+        const { players } = this.props
+        return (players.map(player => {
+            return (
+                <div className="player">
+                    <div key={player.name} className="player">{player.name}</div>
+                    {!player.inGame && <div className="player">{player.type}</div>}
+                </div>
+            )
+        }))
+    }
+
+    render(){
+        return (
+            <div>
+                {this.getPlayers()}
+            </div>
+        )
+    }
+}
+
+export default PlayerList;
