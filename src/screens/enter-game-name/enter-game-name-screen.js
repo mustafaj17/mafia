689e3c3702a05023ba92823cd5ab1281a80618f8
Mustafa J <mustafa.jebbar@telegraph.co.uk>
2018-09-27 23:11:02 +0100

    -created enter name screen -created enter game name screen -added icons/svgs

---
+++ src/screens/enter-game-name/enter-game-name-screen.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Logo from "../../components/logo/logo";
+import './enter-game-name-screen.css';
+import MafiaTextLogo from "../../icons/mafia-text-logo-";
+
+export default class EnterGameNameScreen extends Component{
+    render(){
+        return(
+            <div className='screen enter-name-screen'>
+					<MafiaTextLogo/>
+					<Logo/>
+					<div className="form-holder">
+						<div className="input-title">Enter game name </div>
+						<input type="text" className="input-text-box" value={this.props.inputGameName}
+											 onChange={ e => this.props.updateGameName(e.target.value )}/>
+						{/*{ (this.props.inputUserName && this.props.inputUserName.length > 2) &&*/
+						<div className="ok-btn" onClick={this.createGame}>ok</div>}
+					</div>
+				</div>
+        )
+    }
+}
