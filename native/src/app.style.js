d8556a6734402fa6bb2c514337a1daaf30678d43
Mustafa J <mustafa.jebbar@telegraph.co.uk>
2019-01-22 14:10:21 +0000

    style changes

--- native/src/app.style.js
+++ native/src/app.style.js
@@ -97,11 +97,7 @@ export default StyleSheet.create(
 				backgroundColor: '#f0f0f0',
 				textTransform: 'capitalize'
 			},
-		'loading-text':{
-			letterSpacing:2,
-			color: 'white',
-			marginTop: 10
-		},
+		'loading-text':{ fontSize: 18, color: '#EFEFEF', paddingTop: 18, paddingBottom: 8 , letterSpacing:3, marginTop: 8},
 		'player-draw':
 			{	height: 40,
 				marginRight: 16,
