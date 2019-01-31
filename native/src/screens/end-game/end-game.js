c14792975b1d5ea2fc9b7e4ccb8ba5da35b2079f
mahmoud <mahmoud16h@gmail.com>
2019-01-31 18:14:17 +0000

    Notes in endgame screen

--- native/src/screens/end-game/end-game.js
+++ native/src/screens/end-game/end-game.js
@@ -16,37 +16,42 @@ export default class EndGame extends Component {
         const {players, game} = this.props;
 
         return players.filter(player => player.type === type).map(player => {
-                let isMafia = player.type === 'Mafia';
-                let stylesArray = [];
+            let isMafia = player.type === 'Mafia';
+            let stylesArray = [];
 
-                stylesArray.push(styles['player']);
+            stylesArray.push(styles['player']);
 
-                return (
-                    <PlayerEndGame stylesArray={stylesArray}
-                            player={player}
-                            isMafia={isMafia}
-                            game={game}
-                            endGame
-                    />
-                )
+            return (
+                <PlayerEndGame stylesArray={stylesArray}
+                               player={player}
+                               isMafia={isMafia}
+                               game={game}
+                               endGame
+                />
+            )
         })
     }
 
     render() {
         return (
             <View style={[styles['screen'], styles['game-screen']]}>
-                <Text>GAME HAS ENDED</Text>
-                <Text>Mafias</Text>
-                <ScrollView contentContainerStyle={styles['games']}>
-                    {this.getPlayers("Mafia")}
-                </ScrollView>
-                <Text>Civilians</Text>
-                <ScrollView contentContainerStyle={styles['games']}>
-                    {this.getPlayers("Civilian")}
-                </ScrollView>
-
+                <View>
+                    {/*ADD MAFIA LOGO HEADING*/}
+                    <Text>GAME HAS ENDED</Text>
+                    {/*ADD WHO WINS LOGIC*/}
+                    {/*FIX STYLING*/}
+                    <Text>Mafias</Text>
+                    <ScrollView contentContainerStyle={styles['games']}>
+                        {this.getPlayers("Mafia")}
+                    </ScrollView>
+                    <Text>Civilians</Text>
+                    <ScrollView contentContainerStyle={styles['games']}>
+                        {this.getPlayers("Civilian")}
+                    </ScrollView>
+                </View>
                 <TouchableOpacity onPress={() => this.props.endGame()} style={styles['ready-button-container']}>
-                        <Text>Back to Main Screen</Text>
+                    <Text>Back to Main Screen</Text>
+                    {/*ADD LOGIC TO PLAY AGAIN? OR LEAVE FOR V2*/}
                 </TouchableOpacity>
             </View>
         )
